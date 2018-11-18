69691314ff67e472bb5fbe730d59175af30cb03b
Damien Shaw <damien.shaw@hotmail.co.uk>
2018-11-18 20:26:09 +0000

    feat: added generic
    feat: added generic

--- app.ts
+++ app.ts
@@ -23,3 +23,35 @@ console.log('colour', colour); // 1
 
 let band: Band = Band.Blue; // 101
 console.log('band', band);
+
+// Generic
+function echo<T>(data: T): T {
+  return data;
+}
+
+echo<string>('damien');
+// deliberate mistake below
+// echo<number>('damien'); // expecting a number as an argument
+
+// class SimpleMath<T extends number> {
+//   baseValue: T;
+//   multiply: T;
+//   calculate(): number {
+//     return +this.baseValue * +this.multiply;
+//   }
+// }
+
+class SimpleMath<T extends number | string, U extends number> {
+  baseValue: T | undefined;
+  multiply: U | undefined;
+
+  calculate(): number {
+    let result;
+
+    if (this.baseValue && this.multiply) {
+      result = +this.baseValue * +this.multiply;
+    }
+
+    return  result || 0;
+  }
+}
